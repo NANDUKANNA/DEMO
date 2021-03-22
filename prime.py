ea4491eef70067f8ca41a36e48cc903501cd5c85
nandini_yeturi <nandiniyeturi98@gmail.com>
2021-03-22 17:10:31 +0530

    done

--- prime.py
+++ prime.py
@@ -1,5 +1,8 @@
+import time
 number = int(input("enter a number"))
 if number % 2 == 0:
     print("The number is even")
+    time.sleep(20)
 else:
     print("The number is odd")
+    time.sleep(20)
